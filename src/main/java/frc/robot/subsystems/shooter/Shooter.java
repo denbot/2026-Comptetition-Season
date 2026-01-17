90423ab9deebc001f274d946360a7e4aac3b0185
Stefan <BlyatBoio5@gmail.com>
2026-01-16 17:59:07 -0700

    BROKEN: Added boiler plate Shooter and ShooterIO feat Hailey

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -1,5 +1,45 @@
 package frc.robot.subsystems.shooter;
 
-public class Shooter {
-    
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+
+import java.util.logging.Logger;
+
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Shooter extends SubsystemBase{
+    private final ShooterIO io;
+    private final ShooterIOInputsAutoLogged inputs = new ShooterIOInputsAutoLogged();
+
+    private AngularVelocity shooterVelocitySetpoint = RadiansPerSecond.zero();
+
+    public Shooter(ShooterIO io){
+        this.io = io;
+    }
+
+    @Override
+    public void periodic(){
+        Logger.processInputs("Shooter", inputs)
+    }
+
+    public void setShooterSpeed(AngularVelocity Speed){
+        shooterVelocitySetpoint = Speed;
+    }
+    public void setShooterSpeed(Double Speed){
+        shooterVelocitySetpoint = RotationsPerSecond.of(Speed);
+    }
+
+    public Command runShooterAtSpeed(){
+        return Commands.runOnce(() -> setShooterSpeed(60.0));
+    }
+    public Command stopShooter(){
+        return Commands.runOnce(() -> setShooterSpeed(0.0));
+    }
+
+    public AngularVelocity getShooterVelocitySetpoint(){
+        return shooterVelocitySetpoint;
+    }
 }
--- src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
@@ -1,5 +1,28 @@
 package frc.robot.subsystems.shooter;
 
-public class ShooterIO {
-    
+import static edu.wpi.first.units.Units.Amp;
+import static edu.wpi.first.units.Units.Degree;
+import static edu.wpi.first.units.Units.RevolutionsPerSecond;
+
+import org.littletonrobotics.junction.AutoLog;
+
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Angle;
+
+
+public interface ShooterIO {
+    @AutoLog
+    public static class ShooterIOInputs {
+        public boolean flywheelMotorConnected = false;
+        public AngularVelocity velocityRotPerSec = RevolutionsPerSecond.zero();
+        public double rotatorClosedLoopErrorROt = 0.0;
+        public Current leftCurrentAmps = Amp.zero();
+        public Angle rotatorPositionTOr = Degree.zero();
+    }
+
+    //sets shooter velocity in RevolutionsPerSec
+      public default void updateInputs(ShooterIOInputs inputs) {}
+      public default void setShooterVelocity(AngularVelocity velocity) {}
+
 }
