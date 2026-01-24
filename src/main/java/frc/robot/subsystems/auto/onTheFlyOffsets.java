8baff80875478e4ee8e3af65b6dee82cd1473d12
Stefan Fry <BlyatBoio5@gmail.com>
2026-01-23 20:37:19 -0700

    Refactored previous structure to be simpler

--- src/main/java/frc/robot/subsystems/auto/onTheFlyOffsets.java
+++ src/main/java/frc/robot/subsystems/auto/onTheFlyOffsets.java
@@ -5,30 +5,37 @@ import static edu.wpi.first.units.Units.Degree;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 
-public class onTheFlyOffsets {
-    public enum trenchOffsets{
-        LEFT(0, 0, 0),
-        RIGHT(0, 0, 0);
+public enum onTheFlyOffsets {
+    TRENCH_LEFT(0, 0, 0),
+    TRENCH_RIGHT(0, 0, 0),
 
-        public final Transform2d transform;
+    CLOSE_TOP(neutralOffsets.CLOSE.value, neutralOffsets.TOP.value, 0),
+    CLOSE_MID(neutralOffsets.CLOSE.value, neutralOffsets.MID.value, 0),
+    CLOSE_BOTTOM(neutralOffsets.CLOSE.value, neutralOffsets.BOTTOM.value, 0),
+    CENTER_TOP(neutralOffsets.CENTER.value, neutralOffsets.TOP.value, 0),
+    CENTER_MID(neutralOffsets.CENTER.value, neutralOffsets.MID.value, 0),
+    CENTER_BOTTOM(neutralOffsets.CENTER.value, neutralOffsets.BOTTOM.value, 0),
+    FAR_TOP(neutralOffsets.FAR.value, neutralOffsets.TOP.value, 180),
+    FAR_MID(neutralOffsets.FAR.value, neutralOffsets.MID.value, 180),
+    FAR_BOTTOM(neutralOffsets.FAR.value, neutralOffsets.BOTTOM.value, 180);
 
-        trenchOffsets(double x, double y, double angle){
-            this.transform = new Transform2d(x, y, new Rotation2d(Degree.of(angle)));
-        }
-    }
-
-    public enum neutralZoneOffsets{
-        CENTER(0),
+    private enum neutralOffsets{
         CLOSE(0),
+        CENTER(0),
         FAR(0),
         TOP(0),
         MID(0),
         BOTTOM(0);
 
         public final double value;
-        neutralZoneOffsets(double value){
+        neutralOffsets(double value){
             this.value = value;
         }
     }
 
+    public final Transform2d transform;
+
+    onTheFlyOffsets(double x, double y, double angle){
+        this.transform = new Transform2d(x, y, new Rotation2d(Degree.of(angle)));
+    }
 }
--- src/main/java/frc/robot/subsystems/auto/onTheFlySetpoints.java
+++ src/main/java/frc/robot/subsystems/auto/onTheFlySetpoints.java
@@ -1,68 +1,43 @@
 package frc.robot.subsystems.auto;
 
-import static edu.wpi.first.units.Units.Degree;
-
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Transform2d;
-import frc.robot.subsystems.auto.onTheFlyOffsets.neutralZoneOffsets;
-import frc.robot.subsystems.auto.onTheFlyOffsets.trenchOffsets;
 
-public class onTheFlySetpoints {
+public enum onTheFlySetpoints {
+    // Trench Locations
+    TOP_LEFT(7, 17, onTheFlyOffsets.TRENCH_LEFT),
+    TOP_RIGHT(6, 28, onTheFlyOffsets.TRENCH_RIGHT),
+    BOT_LEFT(12, 22, onTheFlyOffsets.TRENCH_LEFT),
+    BOT_RIGHT(1, 23, onTheFlyOffsets.TRENCH_RIGHT),
+    // Neutral Zone Locations
+    CLOSE_TOP(6, 17, onTheFlyOffsets.CLOSE_TOP),
+    CLOSE_MID(4, 20, onTheFlyOffsets.CLOSE_MID),
+    CLOSE_BOTTOM(1, 22, onTheFlyOffsets.CLOSE_BOTTOM),
+    CENTER_TOP(6, 17, onTheFlyOffsets.CENTER_TOP),
+    CENTER_MID(4, 20, onTheFlyOffsets.CENTER_MID),
+    CENTER_BOTTOM(1, 22, onTheFlyOffsets.CENTER_BOTTOM),
+    FAR_TOP(17, 6, onTheFlyOffsets.FAR_TOP),
+    FAR_MID(20, 4, onTheFlyOffsets.FAR_MID),
+    FAR_BOTTOM(22, 1, onTheFlyOffsets.FAR_BOTTOM);
+    
     public static final AprilTagFieldLayout fieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2026RebuiltAndymark);
 
-    // Trench Locations
-    public enum trenchSetpoints{
-        TOP_LEFT(7, 17, trenchOffsets.LEFT),
-        TOP_RIGHT(6, 28, trenchOffsets.RIGHT),
-        BOT_LEFT(12, 22, trenchOffsets.LEFT),
-        BOT_RIGHT(1, 23, trenchOffsets.RIGHT);
+    public final int redAprilTag;
+    public final int blueAprilTag;
+    public final onTheFlyOffsets offset;
+    public final Pose2d redAlignment;
+    public final Pose2d blueAlignment;
 
-        public final int redAprilTag;
-        public final int blueAprilTag;
-        public final trenchOffsets offset;
-        public final Pose2d redAlignment;
-        public final Pose2d blueAlignment;
-
-        trenchSetpoints(int redAprilTag, int blueAprilTag, trenchOffsets offset){
-            this.redAprilTag = redAprilTag;
-            this.blueAprilTag = blueAprilTag;
-            this.offset = offset;
-            this.redAlignment = getAlignmentPose(redAprilTag, this.offset.transform);
-            this.blueAlignment = getAlignmentPose(blueAprilTag, this.offset.transform);
-        }
+    onTheFlySetpoints(int redAprilTag, int blueAprilTag, onTheFlyOffsets offset){
+        this.redAprilTag = redAprilTag;
+        this.blueAprilTag = blueAprilTag;
+        this.offset = offset;
+        this.redAlignment = getAlignmentPose(redAprilTag, this.offset);
+        this.blueAlignment = getAlignmentPose(blueAprilTag, this.offset);
     }
-     
-    // Neutral Zone Locations
-    public enum neutralSetpoints{
-        CLOSE_TOP(neutralZoneOffsets.CLOSE, neutralZoneOffsets.TOP, 6, 17),
-        CLOSE_MID(neutralZoneOffsets.CLOSE, neutralZoneOffsets.MID, 4, 20),
-        CLOSE_BOTTOM(neutralZoneOffsets.CLOSE, neutralZoneOffsets.BOTTOM, 1, 22),
-        CENTER_TOP(neutralZoneOffsets.CENTER, neutralZoneOffsets.TOP, 6, 17),
-        CENTER_MID(neutralZoneOffsets.CENTER, neutralZoneOffsets.MID, 4, 20),
-        CENTER_BOTTOM(neutralZoneOffsets.CENTER, neutralZoneOffsets.BOTTOM, 1, 22),
-        FAR_TOP(neutralZoneOffsets.FAR, neutralZoneOffsets.TOP, 17, 6),
-        FAR_MID(neutralZoneOffsets.FAR, neutralZoneOffsets.MID, 20, 4),
-        FAR_BOTTOM(neutralZoneOffsets.FAR, neutralZoneOffsets.BOTTOM, 22, 1);
 
-        public final int redAprilTag;
-        public final int blueAprilTag;
-        public final Transform2d offset;
-        public final Pose2d redAlignment;
-        public final Pose2d blueAlignment;
-
-        neutralSetpoints(neutralZoneOffsets xOffset, neutralZoneOffsets yOffset, int redAprilTag, int blueAprilTag){
-            this.redAprilTag = redAprilTag;
-            this.blueAprilTag = blueAprilTag;
-            this.offset = new Transform2d(xOffset.value, yOffset.value, new Rotation2d(Degree.of(0)));
-            this.redAlignment = getAlignmentPose(redAprilTag, this.offset);
-            this.blueAlignment = getAlignmentPose(blueAprilTag, this.offset);
-        }
-    }
-    
-    private static Pose2d getAlignmentPose(int apriltag, Transform2d offset){
-        return fieldLayout.getTagPose(apriltag).get().toPose2d().transformBy(offset);
+    private static Pose2d getAlignmentPose(int apriltag, onTheFlyOffsets offset){
+        return fieldLayout.getTagPose(apriltag).get().toPose2d().transformBy(offset.transform);
     }
 }
